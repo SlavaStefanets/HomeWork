8fc032f7a0e47e318a5cace86880a6de878065db
slavastefanets <jesusbigboss12@gmail.com>
2022-11-13 10:30:44 +0100

    HomeWork 31.10.22  Felix

---
+++ src/felix.java
@@ -0,0 +1,7 @@
+public class felix {
+    public static void main(String[] args) {
+        long FelixLived = 3_011_456_567l; //sek
+        System.out.println(FelixLived / 60 / 60 / 24 / 365);
+        System.out.println("years");
+    }
+}
