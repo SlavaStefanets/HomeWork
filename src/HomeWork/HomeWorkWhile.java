d33516a3c6e5788c075e48170f6c47014121a42a
slavastefanets <jesusbigboss12@gmail.com>
2022-11-20 16:02:11 +0100

    HomeWork While 20.11.22

---
+++ src/HomeWork/HomeWorkWhile.java
@@ -0,0 +1,15 @@
+package HomeWork;
+
+import java.util.Scanner;
+
+public class HomeWorkWhile {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int a = 0;
+        while (number > a) {
+        number--;
+            System.out.println("Я начинаю разбираться с циклами");
+
+    }
+}}
