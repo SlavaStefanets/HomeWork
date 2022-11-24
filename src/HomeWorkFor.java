c6c2922375693d5947f557732fe0723d68726bf5
slavastefanets <jesusbigboss12@gmail.com>
2022-11-24 20:55:11 +0100

    HomeWork Fori 24.11.22

---
+++ src/HomeWorkFor.java
@@ -0,0 +1,11 @@
+public class HomeWorkFor {
+    public static void main(String[] args) {
+        int a = ((1000 / 10) * 2);
+        int b = 1000;
+        for (int i = 1000; i <= b + a ;i++ ) {
+
+            System.out.println(i);
+
+        }
+    }
+}
